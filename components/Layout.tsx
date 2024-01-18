d446f85a8454dfeebe9e23f67aa0f3f40b3d36ee
andw3r <ghgfssc@gmail.com>
2024-01-18 12:19:58 +0200

    theme

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,6 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ReactNode } from "react";
-import { theme } from "../pages/theme";
+import { theme } from "../theme";
 
 interface Props {
   children: ReactNode;
